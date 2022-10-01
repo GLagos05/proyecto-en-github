44140582add34da0fa83c9a3ef0f4278e6121117
Geralexan <g.lagos05@ufromail.cl>
2022-10-01 20:58:15 -0300

    Primer Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Proyecto en GitHub");
+        System.out.println("Este comando fue creado en el notebook");
+    }
+}
